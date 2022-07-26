767b4913a81a0a4fea33acb3981039900966cfa4
Adam <86285095+flyd0g@users.noreply.github.com>
2022-07-26 16:17:25 +0800

    Add variables

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "client_id" {
+  description = "client id"
+}
+
+variable "client_secret" {
+  description = "client secret"
+}
+
+variable "subscription_id" {
+  description = "subscription id"
+}
+
+variable "tenant_id" {
+  description = "tenant id"
+}
