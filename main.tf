400e6650f34677cfc5dd10a5b723e611a7752e1b
Adam <86285095+flyd0g@users.noreply.github.com>
2022-07-26 16:58:27 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ provider "azurerm" {
 }
 
 provider "azuread" {
-    version         =   ">= 0.11"
     client_id       =   var.client_id
     client_secret   =   var.client_secret
     tenant_id       =   var.tenant_id
