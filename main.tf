f282732b00b75dca3f390d394ee8360e70c07661
Adam <86285095+flyd0g@users.noreply.github.com>
2022-07-26 17:02:35 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,12 @@
 terraform {
-    required_providers  {
-        azurerm =   {
-            source  =   "hashicorp/azurerm"
-            version =   "~> 3.0.2"
-        }
+  required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
     }
+    azurerm =   {
+      source  =   "hashicorp/azurerm"
+    }
+  }
 }
 
 provider "azurerm" {
