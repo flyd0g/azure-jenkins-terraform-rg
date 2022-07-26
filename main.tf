ec771d0ac3a0c17246a494e3d8b1ffc014e4bfb9
Adam <86285095+flyd0g@users.noreply.github.com>
2022-07-26 16:53:42 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,12 +2,12 @@ terraform {
     required_providers  {
         azurerm =   {
             source  =   "hashicorp/azurerm"
+            version =   "~> 3.0.2"
         }
     }
 }
 
 provider "azurerm" {
-    version         =   "~> 3.0"
     client_id       =   var.client_id
     client_secret   =   var.client_secret
     subscription_id =   var.subscription_id
