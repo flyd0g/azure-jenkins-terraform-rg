2d6d72c792e47c0073e6fb59a558765e8f5dedda
Adam <86285095+flyd0g@users.noreply.github.com>
2022-07-27 08:49:36 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,28 +1,19 @@
 terraform {
   required_providers {
-    azuread = {
-      source  = "hashicorp/azuread"
-    }
     azurerm =   {
       source  =   "hashicorp/azurerm"
+      version =   "=3.0.0"
     }
   }
 }
 
 provider "azurerm" {
+    features {}
+  
     client_id       =   var.client_id
     client_secret   =   var.client_secret
     subscription_id =   var.subscription_id
     tenant_id       =   var.tenant_id
-    
-    features {}
-}
-
-provider "azuread" {
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    tenant_id       =   var.tenant_id
-    alias           =   "ad"
 }
 
 resource "azurerm_resource_group" "rg" {
